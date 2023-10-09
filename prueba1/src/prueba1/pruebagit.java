d3682dd49ecdc95ed9c1c1da08c1b1a2f85c57f3
miguel <miguel.fernandez.cubero@colegiolitterator.com>
2023-10-09 16:25:14 +0200

    Cambio a de texto random

---
+++ prueba1/src/prueba1/pruebagit.java
@@ -0,0 +1,24 @@
+package prueba1;
+
+import java.util.Scanner;
+
+public class pruebagit {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int num, num2;
+		
+		System.out.println("Introduce un numerooooo y se suma");
+		num = sc.nextInt();
+		System.out.println("Introduce el segundo numero");
+		num2 = sc.nextInt();
+		
+		int resultado = num + num2;
+		
+		System.out.println(resultado);
+		
+	}
+
+}
